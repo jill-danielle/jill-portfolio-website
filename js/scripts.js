11421529efda771bc6951e217231490323ea0450
Jill Danielle <jilldliao@gmail.com>
2019-05-13 23:16:38 +0800

    fixed some errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log("Hi! I'm Jill and I love UX Design")
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
